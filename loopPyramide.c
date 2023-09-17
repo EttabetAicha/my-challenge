4455ba1ac94cf6d9bc3b6dfae7b825858bd8f9b2
aicha <aicha70tabite@gmail.com>
2023-09-17 18:14:49 +0100

    dev my solution challenge pyramide

--- loopPyramide.c
+++ loopPyramide.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
-int main() {
-   int i, j, r;
-   printf("Enter le nombre des ligne: ");
-   scanf("%d", &r);
-    if(r>0){
-    for (i = 1; i <= r; ++i) {
-        for (j = 1; j <= r - i; ++j) {
-            printf(" ");
+int main()
+{
+    int i, j, r;
+    printf("Enter le nombre des ligne: ");
+    scanf("%d", &r);
+    if (r > 0)
+    {
+        for (i = 1; i <= r; ++i)
+        {
+            for (j = 1; j <= r - i; ++j)
+            {
+                printf(" ");
+            }
+            for (j = 1; j <= 2 * i - 1; ++j)
+            {
+                printf("*");
+            }
+            printf("\n");
         }
-      for (j = 1; j <= i; ++j) {
-         printf("*");
-      }
-      printf("\n");
-   }}
-   else if (r < 0) {
-      r = -r; // Convert r to its positive counterpart
-      for (i = r; i >= 1; --i) {
-         for (j = 1; j <= r - i; ++j) {
-            printf(" ");
-         }
-         for (j = 1; j <= 2 * i - 1; ++j) {
-            printf("*");
-         }
-         printf("\n");
-      }}
+    }
+    else if (r < 0)
+    {
+        r = -r;
+        for (i = r; i >= 1; --i)
+        {
+            for (j = 1; j <= r - i; ++j)
+            {
+                printf(" ");
+            }
+            for (j = 1; j <= 2 * i - 1; ++j)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
 
-
-   return 0;
+    return 0;
 }

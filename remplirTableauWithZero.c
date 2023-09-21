3add2d375bf5e4ddd8047c20ce43195efcba311f
aicha <aicha70tabite@gmail.com>
2023-09-21 21:30:58 +0100

    add challenge

---
+++ remplirTableauWithZero.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main(){
+   int tab[10];
+   for(int i=0;i<10;i++){
+        tab[i]=0;
+   }
+   for(int i=0;i<10;i++){
+        printf("%d\t",tab[i]);
+   };
+   
+
+
+    return 0;
+
+
+
+    
+ }
